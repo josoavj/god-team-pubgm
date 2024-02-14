56cc1260f1aaec57bea0276d261e3032b042abb6
Josoa Vonjiniaina <76913187+Josoa886@users.noreply.github.com>
2024-02-14 10:05:09 +0300

    --Revisiting code

---
+++ JS/inscription.js
@@ -0,0 +1,7 @@
+let onClickAction = document.querySelectorAll('button');
+let reset = document.getElementById('reset');
+
+onClickActionButton => {
+    reset.addEventListener('click', (e)=> alert("It's Done"))
+}
+onClickActionButton(); 
